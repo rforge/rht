34f4513c798882ce227dc46d21670d4ce43b0879
beat_hulliger <beat_hulliger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-31 10:03:16 +0000

    delete non-package subdirectories of pkg
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rht@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rhte.Rcheck/00_pkg_src/rhte/R/print.svystat.rob.R
+++
@@ -1,7 +0,0 @@
-print.svystat.rob <-
-function(x, ...){
-   ddigits <- attr(x, "ddigits")
-   m <- cbind(x, sqrt(attr(x, "var")))
-   colnames(m) <- c(attr(x, "statistic"), "SE")
-   print(round(m, ddigits))
-}
--- pkg/rhte/R/vcov.svystat.rob.R
+++
@@ -1,7 +0,0 @@
-vcov.svystat.rob <-
-function(object, ...){
-v <- as.matrix(attr(object, "var"))
-rownames(v) <- names(object)
-colnames(v) <- "Variance"
-return(v)
-}
